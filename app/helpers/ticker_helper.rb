588c9c4a29eff0e067d7883d55e763b4adc21ee5
Glen Smith <gxsplinter@gmail.com>
2015-09-09 17:05:24 +0100

    Stylistic changes
    Changed styling to comply with RuboCop

--- app/helpers/ticker_helper.rb
+++ app/helpers/ticker_helper.rb
@@ -1,23 +1,24 @@
+# This is a helper that will create the unordered list of items for the ticker
 module TickerHelper
   def jquery_ticker(ticker_items)
     @ticker_items = ticker_items
 
-    get_ul
+    ul_content
   end
 
-  def get_ul
-      content_tag(:ul, get_li, id: 'js-news', class: 'js-hidden').html_safe
+  def ul_content
+    content_tag(:ul, li_content, id: 'js-news', class: 'js-hidden').html_safe
   end
 
-  def get_li
+  def li_content
     list_items = []
     @ticker_items.each do |item|
-      list_items << content_tag(:li, get_a(item), class: 'news-item')
+      list_items << content_tag(:li, a_content(item), class: 'news-item')
     end
     list_items.join.html_safe
   end
 
-  def get_a(item)
+  def a_content(item)
     content_tag(:a, item[:text], href: "#{item[:url]}").html_safe
   end
 end
